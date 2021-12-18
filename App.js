59fd8291caab4009bb40194e74dd106394d46190
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-18 16:00:22 +0600

    Added carList component

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import React from "react"
-import CarItem from './assets/components/CarItem';
+import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <CarItem name={"Model X"}
+      <CarsList/>
+      {/* <CarItem name={"Model X"}
         tagline={"Order Online For"}
         image={require("./assets/images/tesla.jpg")}
-        taglineCTA={"Touchless Delivary"} />
+        taglineCTA={"Touchless Delivary"} /> */}
       <StatusBar style='auto' />
     </View>
   );
---
+++ components/CarsList/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, FlatList, Dimensions } from 'react-native';
+import styles from "./styles";
+import CarItem from '../CarItem';
+import cars from "./cars";
+
+const Carlist = (props) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => <CarItem car={item} />}
+        showsHorizontalScrollIndicator={false}
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
+        snapToInterval={Dimensions.get('window').height}
+      />
+
+    </View>
+  );
+};
+
+export default Carlist;
